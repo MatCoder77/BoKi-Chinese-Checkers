dd5216448243e353c5c5337622f8878024b42549
MatCoder77 <matkimzag@wp.pl>
2017-12-09 20:10:28 +0100

    Implemented Client class, added ServerListener class

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -6,12 +6,24 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/**
+ * @author Mateusz
+ *
+ */
+/**
+ * @author Mateusz
+ *
+ */
 public class Client {
 
 	private Socket socket;
+	private Thread serverListener;
 	private BufferedReader input;
 	private PrintWriter output;
-	
+	private int port;
+	private String address;
+	private boolean connected;
+
 	public Client(String host, int port) {
 		try {
 			socket = new Socket(host, port);
@@ -22,4 +34,85 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
+
+	/**
+	 * Returns the state of connection
+	 * 
+	 * @return true if client is connected, false otherwise
+	 */
+	boolean isConnected() {
+		return connected;
+	}
+
+	/**
+	 * Sets connection status
+	 * 
+	 * @param status
+	 */
+	private void setConnected(boolean connectionStatus) {
+		connected = connectionStatus;
+	}
+
+	/**
+	 * Establishes ServerListener for client which will be listening to Responds
+	 * from Server
+	 */
+	public void runServerListener() {
+		serverListener = new Thread(new ServerListener()); // TODO add ServerListener arguments when ServerListener will
+															// be implemented
+		serverListener.start();
+	}
+
+	/**
+	 * Establishes connection between Server and this Client. Initializes Client's
+	 * BufferdReader and PrinterWriter connecting it to Server's OutupStream and
+	 * InputStream. When exception occurs TODO
+	 * 
+	 * @return true if connection was established, false if some kind of problem
+	 *         occurred
+	 */
+	boolean connect() {
+		if (!isConnected()) {
+			try {
+				socket = new Socket(address, port);
+				InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
+				input = new BufferedReader(streamReader);
+				output = new PrintWriter(socket.getOutputStream());
+
+				// TODO send CONNECT_REQUEST
+
+				setConnected(true);
+			} catch (Exception ex) {
+				// TODO handle exception
+			}
+
+			runServerListener();
+
+		} else {
+			// TODO show message "You're already connected"
+		}
+		return true; // TODO error indicating when something will go wrong
+	}
+
+	/**
+	 * Closes connection between Client and Server and frees resources.
+	 * 
+	 * @return true if no problems occurred, false otherwise
+	 */
+	boolean disconnect() {
+		if (isConnected()) {
+			//TODO send DISCONNECT_REQUEST
+			try {
+				socket.close();
+			} catch (Exception ex) {
+				//TODO error handling
+			}
+			setConnected(false);
+		} else {
+			//TODO show message "You're already disconnected"
+		}
+		return true; //TODO error indicating
+	}
+	
+	//TODO sendRequest function
 }
---
+++ src/main/java/client/ServerListener.java
@@ -0,0 +1,11 @@
+package client;
+
+public class ServerListener implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
