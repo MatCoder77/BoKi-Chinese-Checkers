cd998bf81207d2ac6148aa27e4ac8a4f68eb53c3
filipbk <filipbk @ff>
2017-12-09 16:12:42 +0100

    Added classes for server GUI, game, client ant test class for server

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -1,5 +1,42 @@
 package server;
 
-public class ClientHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class ClientHandler implements Runnable {
+	
+	private Socket socket;
+	private BufferedReader input;
+	private PrintWriter output;
+	
+	public ClientHandler(Socket socket) {
+		this.socket = socket;
+		
+		try {
+			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			output = new PrintWriter(socket.getOutputStream(), true);
+			output.println("Welcome to the game!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		
+		try {
+			output.println("Starting the game");
+			while (true) {
+				String command = input.readLine();
+			}
+		} catch (IOException e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,5 +1,40 @@
 package server;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class Server {
+	
+	private static Server instance;
+	private Game game;
+	private ServerSocket serverSocket;
+	private Thread gameThread;
+	private int port;
+	
+
+	private Server (int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+		System.out.println("Server started");
+		game = new Game();
+		gameThread = new Thread(game);
+		gameThread.start();
+	}
+	
+	public static synchronized Server getInstance(int port) {
+		if(instance == null) {
+			try {
+				instance = new Server(port);
+			} catch (IOException e) {
+				System.out.println("Unable to start server");
+				e.printStackTrace();
+			}
+		}
+		return instance;
+	}
+	
+	private void listen() {
+		while (true) {
+			
+		}
+	}
 }
