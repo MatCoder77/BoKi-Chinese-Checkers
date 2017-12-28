410244ce8ec0bc079b010a6b1bc80fbb6b0c0cca
filipbk <filipp973@gmail.com>
2017-12-28 11:50:12 +0100

    GameHandler adds players to game

--- src/main/java/server/GameHandler.java
+++ src/main/java/server/GameHandler.java
@@ -96,6 +96,10 @@ public class GameHandler implements Runnable {
 		if(playersNum == 6) {
 			game = new Game(new BoardSixSix());
 		}
+		
+		for(ClientHandler cl : clients) {
+			game.addPlayer();
+		}
 
 	}
 
