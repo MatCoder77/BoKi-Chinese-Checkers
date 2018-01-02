757ffa7bc332ee0d84a5f975b3990eb32c9bbaf4
MatCoder77 <matkimzag@wp.pl>
2018-01-02 20:32:35 +0100

    EasyBot logic implemented, bot is ready to play

--- src/main/java/server/EasyBot.java
+++ src/main/java/server/EasyBot.java
@@ -13,151 +13,178 @@ public class EasyBot extends BotStrategy {
 	MoveRequest firstMoveInTurn;
 	ArrayList<Point> visitedInThisTurn;
 	MoveRequest lastMove;
+	Pawn currentPawn;
+
+	boolean isMyPawnAtThisPosition(int x, int y) {
+		for (Pawn p : me.getPawns()) {
+			if (p.getLocation().equals(new Point(x, y)))
+				return true;
+		}
+		return false;
+	}
 
 	Point getGoal() {
 		int corner = game.getPlayer(playerID).getCorner();
 		Point goal = null;
 		goal = null;
-		
+
 		if (corner == 4) {
-			goal = new Point(0, 4);
-			/*
-			if(game.getField(0, 4).isEmpty())
+
+			if (currentPawn.isInTarget())
 				goal = new Point(0, 4);
-			else if(game.getField(1, 4).isEmpty())
-				goal = new Point(1, 4);
-			else if(game.getField(1, 5).isEmpty())
-				goal = new Point(1, 5);
-			else if(game.getField(2, 4).isEmpty())
-				goal = new Point(2, 4);
-			else if(game.getField(2, 5).isEmpty())
-				goal = new Point(2, 5);
-			else if(game.getField(2, 6).isEmpty())
-				goal = new Point(2, 6);
-			else if(game.getField(3, 4).isEmpty())
-				goal = new Point(3, 4);
-			else if(game.getField(3, 5).isEmpty())
-				goal = new Point(3, 5);
-			else if(game.getField(3, 6).isEmpty())
-				goal = new Point(3, 6);
-			else if(game.getField(3, 7).isEmpty())
-				goal = new Point(3, 7);*/
+
+			else {
+				if (!isMyPawnAtThisPosition(0, 4))
+					goal = new Point(0, 4);
+				else if (!isMyPawnAtThisPosition(1, 4))
+					goal = new Point(1, 4);
+				else if (!isMyPawnAtThisPosition(1, 5))
+					goal = new Point(1, 5);
+				else if (!isMyPawnAtThisPosition(2, 4))
+					goal = new Point(2, 4);
+				else if (!isMyPawnAtThisPosition(2, 5))
+					goal = new Point(2, 5);
+				else if (!isMyPawnAtThisPosition(2, 6))
+					goal = new Point(2, 6);
+				else if (!isMyPawnAtThisPosition(3, 4))
+					goal = new Point(3, 4);
+				else if (!isMyPawnAtThisPosition(3, 5))
+					goal = new Point(3, 5);
+				else if (!isMyPawnAtThisPosition(3, 6))
+					goal = new Point(3, 6);
+				else if (!isMyPawnAtThisPosition(3, 7))
+					goal = new Point(3, 7);
+			}
 		}
 		if (corner == 5) {
-			goal = new Point(4, 12);
-			/*
-			if(game.getField(4, 12).isEmpty())
+			if (currentPawn.isInTarget())
 				goal = new Point(4, 12);
-			else if(game.getField(4, 11).isEmpty())
-				goal = new Point(4, 11);
-			else if(game.getField(5, 12).isEmpty())
-				goal = new Point(5, 12);
-			else if(game.getField(4, 10).isEmpty())
-				goal = new Point(4, 10);
-			else if(game.getField(5, 11).isEmpty())
-				goal = new Point(5, 11);
-			else if(game.getField(6, 12).isEmpty())
-				goal = new Point(6, 12);
-			else if(game.getField(4, 9).isEmpty())
-				goal = new Point(4, 9);
-			else if(game.getField(5, 10).isEmpty())
-				goal = new Point(5, 10);
-			else if(game.getField(6, 11).isEmpty())
-				goal = new Point(6, 11);
-			else if(game.getField(7, 12).isEmpty())
-				goal = new Point(7, 12);*/
+			else {
+				if (!isMyPawnAtThisPosition(4, 12))
+					goal = new Point(4, 12);
+				else if (!isMyPawnAtThisPosition(4, 11))
+					goal = new Point(4, 11);
+				else if (!isMyPawnAtThisPosition(5, 12))
+					goal = new Point(5, 12);
+				else if (!isMyPawnAtThisPosition(4, 10))
+					goal = new Point(4, 10);
+				else if (!isMyPawnAtThisPosition(5, 11))
+					goal = new Point(5, 11);
+				else if (!isMyPawnAtThisPosition(6, 12))
+					goal = new Point(6, 12);
+				else if (!isMyPawnAtThisPosition(4, 9))
+					goal = new Point(4, 9);
+				else if (!isMyPawnAtThisPosition(5, 10))
+					goal = new Point(5, 10);
+				else if (!isMyPawnAtThisPosition(6, 11))
+					goal = new Point(6, 11);
+				else if (!isMyPawnAtThisPosition(7, 12))
+					goal = new Point(7, 12);
+			}
 		}
 		if (corner == 6) {
-			goal = new Point(12, 16);/*
-			if(game.getField(12, 16).isEmpty())
+			if (currentPawn.isInTarget())
 				goal = new Point(12, 16);
-			else if(game.getField(11, 15).isEmpty())
-				goal = new Point(11, 15);
-			else if(game.getField(12, 15).isEmpty())
-				goal = new Point(12, 15);
-			else if(game.getField(10, 14).isEmpty())
-				goal = new Point(10, 14);
-			else if(game.getField(11, 14).isEmpty())
-				goal = new Point(11, 14);
-			else if(game.getField(12, 14).isEmpty())
-				goal = new Point(12, 14);
-			else if(game.getField(9, 13).isEmpty())
-				goal = new Point(9, 13);
-			else if(game.getField(10, 13).isEmpty())
-				goal = new Point(10, 13);
-			else if(game.getField(11, 13).isEmpty())
-				goal = new Point(11, 13);
-			else if(game.getField(12, 13).isEmpty())
-				goal = new Point(12, 13);*/
+			else {
+				if (!isMyPawnAtThisPosition(12, 16))
+					goal = new Point(12, 16);
+				else if (!isMyPawnAtThisPosition(11, 15))
+					goal = new Point(11, 15);
+				else if (!isMyPawnAtThisPosition(12, 15))
+					goal = new Point(12, 15);
+				else if (!isMyPawnAtThisPosition(10, 14))
+					goal = new Point(10, 14);
+				else if (!isMyPawnAtThisPosition(11, 14))
+					goal = new Point(11, 14);
+				else if (!isMyPawnAtThisPosition(12, 14))
+					goal = new Point(12, 14);
+				else if (!isMyPawnAtThisPosition(9, 13))
+					goal = new Point(9, 13);
+				else if (!isMyPawnAtThisPosition(10, 13))
+					goal = new Point(10, 13);
+				else if (!isMyPawnAtThisPosition(11, 13))
+					goal = new Point(11, 13);
+				else if (!isMyPawnAtThisPosition(12, 13))
+					goal = new Point(12, 13);
+			}
 		}
 		if (corner == 1) {
-			goal = new Point(16, 12);/*
-			if(game.getField(16, 12).isEmpty())
+			if (currentPawn.isInTarget())
 				goal = new Point(16, 12);
-			else if(game.getField(15, 12).isEmpty())
-				goal = new Point(15, 12);
-			else if(game.getField(15, 11).isEmpty())
-				goal = new Point(15, 11);
-			else if(game.getField(14, 12).isEmpty())
-				goal = new Point(14, 12);
-			else if(game.getField(14, 11).isEmpty())
-				goal = new Point(14, 11);
-			else if(game.getField(14, 10).isEmpty())
-				goal = new Point(14, 10);
-			else if(game.getField(13, 12).isEmpty())
-				goal = new Point(13, 12);
-			else if(game.getField(13, 11).isEmpty())
-				goal = new Point(13, 11);
-			else if(game.getField(13, 10).isEmpty())
-				goal = new Point(13, 10);
-			else if(game.getField(13, 9).isEmpty())
-				goal = new Point(13, 9);*/
+			else {
+				if (!isMyPawnAtThisPosition(16, 12))
+					goal = new Point(16, 12);
+				else if (!isMyPawnAtThisPosition(15, 12))
+					goal = new Point(15, 12);
+				else if (!isMyPawnAtThisPosition(15, 11))
+					goal = new Point(15, 11);
+				else if (!isMyPawnAtThisPosition(14, 12))
+					goal = new Point(14, 12);
+				else if (!isMyPawnAtThisPosition(14, 11))
+					goal = new Point(14, 11);
+				else if (!isMyPawnAtThisPosition(14, 10))
+					goal = new Point(14, 10);
+				else if (!isMyPawnAtThisPosition(13, 12))
+					goal = new Point(13, 12);
+				else if (!isMyPawnAtThisPosition(13, 11))
+					goal = new Point(13, 11);
+				else if (!isMyPawnAtThisPosition(13, 10))
+					goal = new Point(13, 10);
+				else if (!isMyPawnAtThisPosition(13, 9))
+					goal = new Point(13, 9);
+			}
 		}
 		if (corner == 2) {
-			goal = new Point(12, 4);/*
-			if(game.getField(12, 4).isEmpty())
+			if (currentPawn.isInTarget())
 				goal = new Point(12, 4);
-			else if(game.getField(12, 5).isEmpty())
-				goal = new Point(12, 5);
-			else if(game.getField(11, 4).isEmpty())
-				goal = new Point(11, 4);
-			else if(game.getField(10, 4).isEmpty())
-				goal = new Point(10, 4);
-			else if(game.getField(11, 5).isEmpty())
-				goal = new Point(11, 5);
-			else if(game.getField(12, 6).isEmpty())
-				goal = new Point(12, 6);
-			else if(game.getField(12, 7).isEmpty())
-				goal = new Point(12, 7);
-			else if(game.getField(11, 6).isEmpty())
-				goal = new Point(11, 6);
-			else if(game.getField(10, 5).isEmpty())
-				goal = new Point(10, 5);
-			else if(game.getField(9, 4).isEmpty())
-				goal = new Point(9, 4);*/
+			else {
+				if (!isMyPawnAtThisPosition(12, 4))
+					goal = new Point(12, 4);
+				else if (!isMyPawnAtThisPosition(12, 5))
+					goal = new Point(12, 5);
+				else if (!isMyPawnAtThisPosition(11, 4))
+					goal = new Point(11, 4);
+				else if (!isMyPawnAtThisPosition(10, 4))
+					goal = new Point(10, 4);
+				else if (!isMyPawnAtThisPosition(11, 5))
+					goal = new Point(11, 5);
+				else if (!isMyPawnAtThisPosition(12, 6))
+					goal = new Point(12, 6);
+				else if (!isMyPawnAtThisPosition(12, 7))
+					goal = new Point(12, 7);
+				else if (!isMyPawnAtThisPosition(11, 6))
+					goal = new Point(11, 6);
+				else if (!isMyPawnAtThisPosition(10, 5))
+					goal = new Point(10, 5);
+				else if (!isMyPawnAtThisPosition(9, 4))
+					goal = new Point(9, 4);
+			}
 		}
 		if (corner == 3) {
-			goal = new Point(4, 0);/*
-			if(game.getField(4, 0).isEmpty())
+			if (currentPawn.isInTarget())
 				goal = new Point(4, 0);
-			else if(game.getField(4, 1).isEmpty())
-				goal = new Point(4, 1);
-			else if(game.getField(5, 1).isEmpty())
-				goal = new Point(5, 1);
-			else if(game.getField(6, 2).isEmpty())
-				goal = new Point(6, 2);
-			else if(game.getField(5, 2).isEmpty())
-				goal = new Point(5, 2);
-			else if(game.getField(4, 2).isEmpty())
-				goal = new Point(4, 2);
-			else if(game.getField(4, 3).isEmpty())
-				goal = new Point(4, 3);
-			else if(game.getField(5, 3).isEmpty())
-				goal = new Point(5, 3);
-			else if(game.getField(6, 3).isEmpty())
-				goal = new Point(6, 3);
-			else if(game.getField(7, 3).isEmpty())
-				goal = new Point(7, 3);*/
+			else {
+				if (!isMyPawnAtThisPosition(4, 0))
+					goal = new Point(4, 0);
+				else if (!isMyPawnAtThisPosition(4, 1))
+					goal = new Point(4, 1);
+				else if (!isMyPawnAtThisPosition(5, 1))
+					goal = new Point(5, 1);
+				else if (!isMyPawnAtThisPosition(6, 2))
+					goal = new Point(6, 2);
+				else if (!isMyPawnAtThisPosition(5, 2))
+					goal = new Point(5, 2);
+				else if (!isMyPawnAtThisPosition(4, 2))
+					goal = new Point(4, 2);
+				else if (!isMyPawnAtThisPosition(4, 3))
+					goal = new Point(4, 3);
+				else if (!isMyPawnAtThisPosition(5, 3))
+					goal = new Point(5, 3);
+				else if (!isMyPawnAtThisPosition(6, 3))
+					goal = new Point(6, 3);
+				else if (!isMyPawnAtThisPosition(7, 3))
+					goal = new Point(7, 3);
+			}
 		}
 		return goal;
 	}
@@ -171,16 +198,15 @@ public class EasyBot extends BotStrategy {
 		int minDistace = Integer.MAX_VALUE;
 		int newDistance;
 		int randomPawnIndex;
-		Pawn p;
 		do {
 			randomPawnIndex = ThreadLocalRandom.current().nextInt(0, 10);
-			p = me.getPawns().get(randomPawnIndex);
-			from = p.getLocation();
+			currentPawn = me.getPawns().get(randomPawnIndex);
+			from = currentPawn.getLocation();
 			possibleMoves = game.checkValidMoves(playerID, from);
-			possibleMoves = processPossibleMoves(p, possibleMoves);
-		} while (p.getLocation().equals(getGoal()) || possibleMoves.isEmpty());
+			possibleMoves = processPossibleMoves(currentPawn, possibleMoves);
+		} while (currentPawn.getLocation().equals(getGoal()) || possibleMoves.isEmpty());
 		// for(Pawn p : me.getPawns()) {
-		// Pawn savedState = new Pawn(p);		
+		// Pawn savedState = new Pawn(p);
 		for (Point to : possibleMoves) {
 			newDistance = evaluate(to);
 			if (minDistace > newDistance) {
@@ -234,22 +260,21 @@ public class EasyBot extends BotStrategy {
 		return (int) Math.hypot(Math.abs(getGoal().x - pos.x), Math.abs(getGoal().y - pos.y));
 
 	}
-	
+
 	ArrayList<Point> processPossibleMoves(Pawn p, ArrayList<Point> possible) {
-		
-		if(p.isInTarget()) {
+
+		if (p.isInTarget()) {
 			int currentDistance = evaluate(p.getLocation());
 			int distanceAfterMove = Integer.MAX_VALUE;
 			ArrayList<Point> processedMoves = new ArrayList<>();
-			for(Point move : possible) {
+			for (Point move : possible) {
 				distanceAfterMove = evaluate(move);
-				if(distanceAfterMove < currentDistance) {
+				if (distanceAfterMove < currentDistance) {
 					processedMoves.add(move);
 				}
 			}
 			return processedMoves;
-		}
-		else
+		} else
 			return possible;
 	}
 
